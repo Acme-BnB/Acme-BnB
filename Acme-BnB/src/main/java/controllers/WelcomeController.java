c009f6c281c7c7222528d9f0a01678126628d1c5
Ruben <ruben_barr@hotmail.com>
2017-03-06 20:23:53 +0100

    Modifico tamaño imagen, quito saludos John Doe, y personalizamos el saludo por usuario

--- Acme-BnB/src/main/java/controllers/WelcomeController.java
+++ Acme-BnB/src/main/java/controllers/WelcomeController.java
@@ -18,10 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
 @RequestMapping("/welcome")
 public class WelcomeController extends AbstractController {
-
+	
 	// Constructors -----------------------------------------------------------
 	
 	public WelcomeController() {
@@ -31,16 +32,14 @@ public class WelcomeController extends AbstractController {
 	// Index ------------------------------------------------------------------		
 
 	@RequestMapping(value = "/index")
-	public ModelAndView index(@RequestParam(required=false, defaultValue="John Doe") String name) {
+	public ModelAndView index(@RequestParam(required=false) String name) {
 		ModelAndView result;
 		SimpleDateFormat formatter;
 		String moment;
-		
 		formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		moment = formatter.format(new Date());
 				
 		result = new ModelAndView("welcome/index");
-		result.addObject("name", name);
 		result.addObject("moment", moment);
 
 		return result;
--- Acme-BnB/src/main/webapp/views/welcome/index.jsp
+++ Acme-BnB/src/main/webapp/views/welcome/index.jsp
@@ -16,6 +16,7 @@
 <%@taglib prefix="security" uri="http://www.springframework.org/security/tags"%>
 <%@taglib prefix="display" uri="http://displaytag.sf.net"%>
 
-<p><spring:message code="welcome.greeting.prefix" /> ${name}<spring:message code="welcome.greeting.suffix" /></p>
-
+<security:authorize access="isAuthenticated()" >
+	<p><spring:message code="welcome.greeting.prefix" /> ${pageContext.request.userPrincipal.name} <spring:message code="welcome.greeting.suffix" /></p>
+</security:authorize>
 <p><spring:message code="welcome.greeting.current.time" /> ${moment}</p> 
